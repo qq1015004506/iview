f6db5612fc11e5de56de80506520d24eaaad9d04
qzy0629 <1015004506@qq.com>
2019-04-15 11:47:57 +0800

    开始编写编码人员的功能

---
+++ src/views/coder/CodeVersion.vue
@@ -0,0 +1,35 @@
+<template>
+    <dev-article>
+        <div style="padding: 32px 64px;">
+            <Timeline>
+                <TimelineItem v-for="file in data" color="green">{{file.commit}}</TimelineItem>
+            </Timeline>
+        </div>
+    </dev-article>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        name: "CodeVersion",
+        data() {
+            return {
+                data:[],
+            }
+        },
+        methods:{
+            getData() {
+                axios.get("http://localhost:8888/file/task/2").then(res=>{
+                    this.data = res.data
+                })
+            }
+        },
+        mounted(){
+            this.getData()
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/coder/TaskManage.vue
@@ -0,0 +1,250 @@
+<template>
+    <dev-article>
+        <div style="padding: 32px 64px;">
+            <Table :columns="columns" :data="data" :loading="loading" border size="small"></Table>
+            <div style="text-align: center; margin: 16px 0">
+                <Page :total="total"
+                      :current.sync="current"
+                      show-sizer
+                      @on-change="getData"
+                      @on-page-size-change="handleChangeSize"></Page>
+            </div>
+            <Modal v-model="openUpload" title="添加成员">
+                <Form>
+                    <FormItem label="提交信息">
+                        <Input v-model="fileInfo.commit" type="textarea" :rows="row"/>
+                    </FormItem>
+                    <FormItem label="上传文件">
+                        <br>
+                        <Upload
+                                ref="upload"
+                                :data="fileInfo"
+                                :before-upload="handleBeforeUpload"
+                                :on-success="handleSuccess"
+                                :on-error="handleError"
+                                type="drag"
+                                action="http://localhost:8888/file">
+                            <div style="padding: 20px 0">
+                                <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+                                <p>Click or drag files here to upload</p>
+                            </div>
+                        </Upload>
+                    </FormItem>
+                </Form>
+            </Modal>
+        </div>
+    </dev-article>
+</template>
+
+
+<script>
+    import axios from 'axios'
+    export default {
+        name: "TaskManage",
+        data() {
+            return{
+                row:4,
+                fileInfo:{
+                    staffId:0,
+                    taskId:0,
+                    commit:'',
+                },
+                openUpload:false,
+                queryGroup:[],
+
+                query:{
+                    name:null,
+                    group:null,
+                    staffName:null,
+                },
+                data:[],
+                columns: [
+                    {
+                        title: '任务名称',
+                        key: 'name'
+                    },
+                    {
+                        title: '任务信息',
+                        key: 'description'
+                    },
+                    {
+                        title: '开始时间',
+                        key: 'startTime',
+                        render: (h, {row, index}) => {
+                            return h('Time',{
+                                props:{
+                                    time: (new Date(row.startTime)).getTime() - 86400 * 3 * 1000,
+                                    type:'date'
+                                }
+                            })
+                        }
+                    },
+                    {
+                        title: '结束时间',
+                        key: 'endTime',
+                        render: (h, {row, index}) => {
+                            return h('Time',{
+                                props:{
+                                    time: (new Date(row.endTime)).getTime() - 86400 * 3 * 1000,
+                                    type:'date'
+                                }
+                            })
+                        }
+                    },
+                    {
+                        title: '状态',
+                        key: 'stage',
+                        render: (h, {row, index}) => {
+
+                            let edit;
+
+                            if (row.stage === 1 || row.stage === "1") {
+                                edit = h('Badge', {
+                                    props: {
+                                        status: 'default',
+                                        text: '未分配'
+                                    }
+                                })
+                            } else if (row.stage === 2 || row.stage === "2") {
+                                edit = h('Badge', {
+                                    props: {
+                                        status: 'processing',
+                                        text: '开发中'
+                                    }
+                                })
+                            } else if (row.stage === 3 || row.stage === "3") {
+                                edit = h('Badge', {
+                                    props: {
+                                        status: 'warning',
+                                        text: '未通过测试'
+                                    }
+                                })
+                            }else if (row.stage === 4 || row.stage === "4") {
+                                edit = h('Badge', {
+                                    props: {
+                                        status: 'success',
+                                        text: '任务完成'
+                                    }
+                                })
+                            }else if (row.stage === 5 || row.stage === "5") {
+                                edit = h('Badge', {
+                                    props: {
+                                        status: 'error',
+                                        text: '任务过期'
+                                    }
+                                })
+                            }
+
+                            return h('div', [edit]);
+
+                        }
+                    },
+                    {
+                        title: 'Action',
+                        key: 'action',
+                        width: 200,
+                        align: 'center',
+                        render: (h, params) => {
+                            console.log(params)
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'primary',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.fileInfo.taskId = params.row.id;
+                                            this.fileInfo.staffId = params.row.staffId;
+                                            this.fileInfo.commit = '';
+                                            this.$refs.upload.clearFiles();
+                                            this.openUpload = true;
+                                        }
+                                    }
+                                }, '提交任务'),
+                                h('Button', {
+                                    props: {
+                                        type: 'success',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.$router.push({
+                                                path: '/updateTask',
+                                                query:{
+                                                    id:params.row.id,
+                                                }
+                                            })
+                                        }
+                                    }
+                                }, '查看详情'),
+                            ]);
+                        }
+                    }
+                ],
+
+                loading: false,
+                current: 1,
+                size:10,
+                total:0,
+            }
+        },
+        methods:{
+            handleError(error){
+                this.$Message.success("上传失败:" + error)
+                this.openUpload = false;
+            },
+            handleSuccess() {
+                this.$Message.success("上传成功")
+                this.openUpload = false;
+            },
+            getData(){
+
+                if(this.loading) return;
+                this.loading = true;
+                let path = 'http://localhost:8888/task/staff/'+7;
+
+                axios.get(path).then(res=>{
+                    this.data = res.data;
+                });
+                console.log(this.data)
+                this.loading = false;
+            },
+            handleChangeSize(val) {
+                this.size = val;
+                this.$nextTick(()=>{
+                    this.getData();
+                })
+            },
+            handleBeforeUpload() {
+                if (this.fileInfo.commit.length === 0) {
+                    this.$Notice.warning({title:"请输入提交信息"});
+                    return false;
+                }
+                return true;
+            },
+            queryData(){
+                this.getData()
+            },
+            handleQueryReset(){
+                this.query.group = null;
+                this.query.name = null;
+                this.query.staffName = null;
+                this.getData();
+            }
+        },
+        mounted(){
+            this.getData();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
